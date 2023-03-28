aeb465c65cbe05a9978a9b5c8bdf8ef3f9ed3e6a
Matias <86579814+MatiasM12@users.noreply.github.com>
2023-03-28 13:35:36 -0300

    Update Auto.java

--- src/main/java/core/Auto.java
+++ src/main/java/core/Auto.java
@@ -3,17 +3,17 @@ package core;
 public class Auto {
     // Variables de instancia para la clase Persona
     private String nombre;
-    private int año;
+    private int anio;
     private String marca;
 
     // Constructor para crear una nueva instancia de Persona
-    public Auto(String nombre, int año, String marca) {
+    public Auto(String nombre, int anio, String marca) {
         this.nombre = nombre;
-        this.año = año;
+        this.anio = anio;
         this.marca = marca;
     }
 
-    // Métodos para obtener y establecer los valores de las variables de instancia
+    // MÃ©todos para obtener y establecer los valores de las variables de instancia
     public String getNombre() {
         return nombre;
     }
@@ -23,10 +23,10 @@ public class Auto {
     }
 
 
-    // Método para imprimir los detalles de un auto
+    // MÃ©todo para imprimir los detalles de un auto
     public void imprimirDetalles() {
         System.out.println("Nombre: " + nombre);
-        System.out.println("Año: " + año);
+        System.out.println("Anio: " + aÃ±o);
         System.out.println("Marca: " + marca);
     }
 }
