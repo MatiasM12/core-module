cb39c26d937667778e667b43392beed1707903f5
Matias <86579814+MatiasM12@users.noreply.github.com>
2023-03-28 22:55:37 -0300

    Update and rename Auto2.java to Casa.java

---
+++ src/main/java/core/Casa.java
@@ -0,0 +1,38 @@
+public class Casa {
+    // Atributos
+    private int numHabitaciones;
+    private double area;
+    private String direccion;
+
+    // Constructor
+    public Casa(int numHabitaciones, double area, String direccion) {
+        this.numHabitaciones = numHabitaciones;
+        this.area = area;
+        this.direccion = direccion;
+    }
+
+    // Métodos
+    public int getNumHabitaciones() {
+        return numHabitaciones;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setNumHabitaciones(int numHabitaciones) {
+        this.numHabitaciones = numHabitaciones;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
