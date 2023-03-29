2ef61648479af119b13873cde417670f54f0e0ab
Felipe Corso <pipecorso79@gmail.com>
2023-03-29 10:28:10 -0300

    Nueva clase principal

---
+++ src/main/java/core/RepoTestFinder.java
@@ -0,0 +1,12 @@
+package core;
+
+public class RepoTestFinder {
+	//no se si me gusta el nombre, pero habia que arrancar por algo
+	
+	String restultadoTest;
+	
+	public void RepoTestFinder() {
+		this.restultadoTest = "Hola mundo! Esto es un resultado de los test";
+	}
+
+}
