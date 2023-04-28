2e7bc3c1c71bcd2dc2612fe9a3431ea257789452
Maty CABJ <matias.agustin.morales@gmail.com>
2023-04-27 21:09:25 -0300

    diego

---
+++ src/main/java/core/Checker.java
@@ -0,0 +1,12 @@
+package core;
+
+import java.net.URL;
+import java.util.Date;
+
+public abstract class Checker {
+    protected Observer observer;
+    public void addObserver(Observer observer){
+        this.observer = observer;
+    }
+    public abstract void check(String path);
+}
---
+++ src/main/java/core/Finder.java
@@ -0,0 +1,9 @@
+package core;
+
+import netscape.javascript.JSObject;
+
+import java.net.URL;
+
+public interface Finder {
+    public ReportResult find(String path);
+}
