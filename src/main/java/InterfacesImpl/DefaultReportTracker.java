5e642ba62fc26027d5b20b69463581e46b0fa836
“Nicolas2k19” <nicolas2k19@outlook.com>
2023-05-01 18:22:48 -0300

    rompiendo todo

--- src/main/java/InterfacesImpl/DefaultReportTracker.java
+++ src/main/java/InterfacesImpl/DefaultReportTracker.java
@@ -16,9 +16,11 @@ public class DefaultReportTracker implements Tracker {
     	System.out.println(rd);
     	
         Map<String, Boolean> map = new HashMap<>();
-        map.put("test1", rd.nextBoolean());
+        map.put("test1", null);
         map.put("test2", rd.nextBoolean());
         map.put("test3", rd.nextBoolean());
+        map.put("test4", rd.nextBoolean());
+        map.put("test5", rd.nextBoolean());
         ReportResult ret = new ReportResult("US1", map);
         return ret;
     }
