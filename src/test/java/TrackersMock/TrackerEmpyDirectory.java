ec83f0bd4e7adf8fd5f14b3ac6ddc1aeee149cdc
Maty CABJ <matias.agustin.morales@gmail.com>
2023-05-05 11:37:17 -0300

    criterios de us2

--- src/test/java/TrackersMock/TrackerEmpyDirectory.java
+++ src/test/java/TrackersMock/TrackerEmpyDirectory.java
@@ -1,5 +1,18 @@
 package TrackersMock;
 
-public class TrackerEmpyDirectory {
+import java.io.FileNotFoundException;
+
+import javax.annotation.processing.FilerException;
+
+import core.ReportResult;
+import core.Tracker;
+
+public class TrackerEmpyDirectory implements Tracker{
+
+	@Override
+	public ReportResult find(String path) throws FileNotFoundException, FilerException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
