2745c1f246cca093477b8beeeab01f2f89e1823e
DiegoLopezPaez <diegolopezpaez3464@gmail.com>
2023-05-06 20:30:28 -0300

    Cambio

--- src/main/java/core/InitCore.java
+++ src/main/java/core/InitCore.java
@@ -7,7 +7,7 @@ import java.util.Set;
 public class InitCore {
     public NewCore init(String trackersPath, String trackerImpl, String path) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, IOException, InvocationTargetException {
         TrackerFinder trackerFinder = new TrackerFinder();
-        Set<Tracker> trackerSet = trackerFinder.findTrackers(trackersPath);
+        //Set<Tracker> trackerSet = trackerFinder.findTrackers(trackersPath);
 
         NewCore core = new NewCore();
 
