2fe724ae254af5f4b65b8b4e9951e3190e06953a
“Nicolas2k19” <nicolas2k19@outlook.com>
2023-05-19 01:05:20 -0300

    AGREGADAS COSAS

--- src/test/java/core/US1.java
+++ src/test/java/core/US1.java
@@ -1,73 +1,32 @@
 package core;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.FileNotFoundException;
 import java.lang.reflect.InvocationTargetException;
 
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
-public class US1 {
+class US1 {
+
+	private static CoreInit init;
+	private  static ObservableTestSummary testSummary;
 	
-    
-	/*Inicializaciones*/
-    private  CoreInit init;
-  
-    private  ObservableTestSummary testSummary;
-    
-    /*Paths y implementaciones de tracker*/
-    private  String findersImplPath = "C:\\Users\\Nicol\\git\\core-module12\\bin\\main\\core";
-    private  String trackerImp = "TrackerHub";
-    private  String trackerImp1 = "trackers.TrackerTestSummary";
-    private  String url = "www.github.com";
-   
-    @BeforeAll
-    public void escenario1() throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, FileNotFoundException  {
-    	init = new CoreInit();
+	@BeforeAll
+	static void escenario1() throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, FileNotFoundException {
+		init = new CoreInit();
     	testSummary =(ObservableTestSummary)init.init("plugins", "trackers.TrackerTestSummary");
-    	System.out.println(testSummary);
-    
-    }
-    @Test 
-    public void nullTestSummary(){
-    	assertTrue(testSummary!=null);
-    }
-    @Test 
-    public void addTest(){
-    	
-    }
-    
-    @Test 
-    public void changeOfState() {
-    	
-    
-    }
-    @Test 
-    public void noChanges()  {
-    	
-    }
-    
-    @Test 
-    public void usChanged()  {
-    		
-    }
-    
-    @Test 
-    public void usResultChanged()  {
-    		
-    }
-    
-    
-    @Test 
-    public void noUS()  {
-    	
-    }
-    
-    @Test 
-    public void oneUS() {
-    	
-    }
+	}
+	
+	@Test
+	void test() throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, FileNotFoundException {
+	//	init = new CoreInit();
+    //	testSummary =(ObservableTestSummary)init.init("plugins", "trackers.TrackerTestSummary");
+   	assertFalse(testSummary==null);
+	}
+	
+	
+	
 
-    
 }
