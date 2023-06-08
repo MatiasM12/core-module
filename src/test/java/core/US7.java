f87d6cbabad9a9f9af9cc767324fc6eda7b5295e
“Nicolas2k19” <nicolas2k19@outlook.com>
2023-06-08 00:03:20 -0300

    Subidos todos los tests, todos funcionan correctamente, todo green, re greeen

---
+++ src/test/java/core/US7.java
@@ -0,0 +1,43 @@
+package core;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import Interfaces.Observable;
+import Interfaces.Observer;
+import Interfaces.TestSummary;
+import coreInicialization.Core;
+import mocks.ObAT;
+
+class US7 {
+
+	public static Core c;
+	public static TestSummary  ts;
+	public static Observer  o;
+	
+	@BeforeAll
+	public static void escenario() throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, IOException {
+	String [] args = new String[]  {"www.github/proyecto1/","US1","DefaultTS"};
+	c  = new Core();
+	Observable obv = c.init(args);
+	o = new ObAT(obv);
+	ts = ((ObAT)o).getTS();
+	
+	}
+	@Test
+	 void testSummaryAnterior () throws FileNotFoundException, ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
+		assertEquals(c.initImplementation("TSInexistente","www.github/proyecto1/","US1").isSuccess(),false);
+		assertEquals(ts.equals(((ObAT)o).getTS()),true);
+	}
+
+	
+	
+
+
+}
