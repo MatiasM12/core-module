3fdae3b73f51c181072873f510380a6316085425
Maty CABJ <matias.agustin.morales@gmail.com>
2023-06-08 17:39:28 -0300

    test

--- src/test/java/core/US1.java
+++ src/test/java/core/US1.java
@@ -2,7 +2,7 @@ package core;
 
 import coreInicialization.Core;
 import mocks.ObAT;
-import mocks.TSStub;
+import mocks.TSStub; 
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
