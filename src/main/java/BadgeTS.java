61e99435ba6ed76a7021adcda87da319faa3023b
“Nicolas2k19” <nicolas2k19@outlook.com>
2023-06-09 06:12:46 -0300

    resolver?

--- src/main/java/BadgeTS.java
+++ src/main/java/BadgeTS.java
@@ -26,7 +26,7 @@ import com.google.gson.JsonElement;
 
 public class BadgeTS extends OriginTS implements HttpHandler {
 
-	HttpServer server;
+	//HttpServer server;
 	
 	public BadgeTS(TestSummary ts) {
 		super(ts);
@@ -148,12 +148,13 @@ public class BadgeTS extends OriginTS implements HttpHandler {
 		
 		
 		try {
+			HttpServer server;
 			System.out.println("Todo salio bien");
-			this.server.stop(0);
-			this.server = HttpServer.create(new InetSocketAddress(8095), 0);
-			this.server.createContext("/mi-endpoint", this);
-	        this.server.setExecutor(null);
-	        this.server.start();
+			//server.stop(0);
+			server = HttpServer.create(new InetSocketAddress(8095), 0);
+			server.createContext("/mi-endpoint", this);
+			server.setExecutor(null);
+			server.start();
 	        System.out.println("Servidor iniciado en el puerto 8095");
 			return true;
 		} catch (IOException e) {
